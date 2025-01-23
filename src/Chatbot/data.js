71c3459df059aae87652e82c8439174379c34afb
Shantanu Waikar <shantanuwaikar10@gmail.com>
2025-01-23 14:58:30 +0530

    first commit

---
+++ src/Chatbot/data.js
@@ -0,0 +1,40 @@
+const data = {
+    kid: {
+        boy: {
+            name: "Brushed-inside hoodie",
+            imageUrl: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fd5%2F79%2Fd579f596cf3a276490e47c7d954681575ea7a831.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bkids_boys_clothing_jumperssweatshirts_hoodies%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]",
+            link: "https://www2.hm.com/en_in/productpage.0542551073.html?pr_oyster=hfrHQ3ku7Q&srsltid=AfmBOoqzfFYdc1j0hK9a6wyRY5vYT9YMoUouTd7P3zYjs1qTmgfzyICqncM&gQT=1"
+        },
+        girl: {
+            name: "2-piece hoodie and leggings set",
+            imageUrl: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F3d%2F75%2F3d75486898739fcb0874c8eb3a153a2045e92727.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]",
+            link: "https://www2.hm.com/en_in/productpage.1057813028.html?pr_oyster=f6n3JdMIk7&srsltid=AfmBOooTY019a703e-7C4jo8ft-i_4QcKmFOjjHM0-jtzaKT58gEa8jb2FQ&gQT=1"
+        }
+    },
+    teenagers: {
+        boy: {
+            name: "Training AEROREADY Heather Tee",
+            imageUrl: "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/dbac7e72253b46fab16f643f716eab7e_9366/Training_AEROREADY_Heather_Tee_Kids_Black_IR6213_01_laydown.jpg",
+            link: "https://www.adidas.co.in/training-aeroready-heather-tee-kids/IR6213.html"
+        },
+        girl: {
+            name: "Trendy Peach Short Western Dress",
+            imageUrl: "https://images.meesho.com/images/products/441408357/qnnem_512.webp",
+            link: "https://www.meesho.com/trendy-peach-short-western-dress-for-girls-with-stylish-square-neck-bodycon-dress-for-kids/p/7aswv9?gQT=1"
+        }
+    },
+    adults: {
+        boy: {
+            name: "Men Charcoal Black Half Sleeve Plain T-Shirt",
+            imageUrl: "https://triprindia.com/cdn/shop/files/TCHBLRNBBD-PLAIN1_700x.jpg?v=1727434252",
+            link: "https://triprindia.com/products/full-navy-half-sleeve-t-shirt?variant=45056491454692&country=IN&currency=INR&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&srsltid=AfmBOoqIfWo6Tmi6JUuKavi2-lKeoHwZuvXTAKEyZTnS3T80ippwiRBw7DU&gQT=1"
+        },
+        girl: {
+            name: "Leriya Fashion Western Dress",
+            imageUrl: "https://m.media-amazon.com/images/I/61RrIEfkN2L._SX679_.jpg",
+            link: "https://www.amazon.in/Leriya-Fashion-Western-Collared-Dresses/dp/B0C6GR3XPZ?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&psc=1&smid=A1WYWER0W24N8S&gQT=1"
+        }
+    }
+}
+
+export default data;
